bffd64baaebde125ed8a0cb334e59906f2bc5200
hennickc <hennickc@amazon.com>
2025-07-30 16:50:17 -0700

    Skip throttling when time-to-reset is less than the available CPU time

--- src/main.rs
+++ src/main.rs
@@ -145,8 +145,10 @@ async fn do_checks<S: DirectStateStore, T: ReasonablyRealtime, U: RateLimitingMi
         if cpu_tenths_spent_after >= 5400 {
             let (_, [minutes_to_reset, seconds_within_minute_to_reset]) = time_to_reset_regex.captures_iter(&resources_text).next().unwrap().extract();
             let seconds_to_reset = minutes_to_reset.parse::<u64>().unwrap() * 60 + seconds_within_minute_to_reset.parse::<u64>().unwrap();
-            warn!("Throttling {} seconds due to high server CPU usage", seconds_to_reset);
-            sleep(Duration::from_secs(seconds_to_reset)).await;
+            if seconds_to_reset * 10 > (6000 - cpu_tenths_spent_after) {
+                warn!("Throttling {} seconds due to high server CPU usage", seconds_to_reset);
+                sleep(Duration::from_secs(seconds_to_reset)).await;
+            }
         }
         cpu_tenths_spent_before = cpu_tenths_spent_after;
     }
