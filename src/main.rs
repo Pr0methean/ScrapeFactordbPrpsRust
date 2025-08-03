31f8a1722fb28fc9d0003a8126ffa631995d2918
hennickc <hennickc@amazon.com>
2025-08-02 17:59:47 -0700

    Tweak new-search-page thresholds

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,8 @@ const RESULTS_PER_PAGE: usize = 64;
 const MIN_DIGITS_IN_PRP: u64 = 300;
 const CHECK_ID_URL_BASE: &str = "https://factordb.com/index.php?open=Prime&ct=Proof&id=";
 const TASK_BUFFER_SIZE: usize = 2 * RESULTS_PER_PAGE;
-const MIN_CAPACITY_AT_START_OF_SEARCH: usize = RESULTS_PER_PAGE;
-const MIN_CAPACITY_AT_RESTART: usize = TASK_BUFFER_SIZE - RESULTS_PER_PAGE;
+const MIN_CAPACITY_AT_START_OF_SEARCH: usize = (3 * RESULTS_PER_PAGE) / 4;
+const MIN_CAPACITY_AT_RESTART: usize = TASK_BUFFER_SIZE - (3 * RESULTS_PER_PAGE) / 4;
 #[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Hash)]
 #[repr(C)]
 struct PrpChecksTask {
