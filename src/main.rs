f18c25b07228c6e6c3ef3ed1ee47e93d731108b3
hennickc <hennickc@amazon.com>
2025-08-08 13:21:30 -0700

    Handle unknown retries if handling a first unknown was successful

--- src/main.rs
+++ src/main.rs
@@ -238,7 +238,18 @@ async fn do_checks<
                     &resources_regex,
                     &mut bases_before_next_cpu_check,
                 ).await;
-                try_handle_unknown(&mut retry, &sender, &http, &mut filter, &u_status_regex, &mut task_bytes, id, wait_until, source_file).await;
+                if try_handle_unknown(&mut retry, &sender, &http, &mut filter, &u_status_regex, &mut task_bytes, id, wait_until, source_file).await {
+                    loop {
+                        let Some(CheckTask { id, details: CheckTaskDetails::U { wait_until, source_file } })
+                            = retry.pop_front() else {
+                            break;
+                        };
+                        rps_limiter.until_ready().await;
+                        if !try_handle_unknown(&mut retry, &sender, &http, &mut filter, &u_status_regex, &mut task_bytes, id, wait_until, source_file).await {
+                            break;
+                        }
+                    }
+                }
             }
         }
     }
