f95fa026394b8a0bfdaafedb7058e399f3a69b84
hennickc <hennickc@amazon.com>
2025-09-11 11:56:35 -0700

    Process composites when we're unable to do anything else

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@
 #![feature(file_buffered)]
 
 use expiring_bloom_rs::{ExpiringBloomFilter, FilterConfigBuilder, InMemoryFilter};
-use governor::clock::{DefaultClock, ReasonablyRealtime};
-use governor::middleware::RateLimitingMiddleware;
-use governor::state::{DirectStateStore, InMemoryState, NotKeyed};
-use governor::{NotUntil, Quota, RateLimiter};
+use governor::clock::{DefaultClock};
+use governor::state::{InMemoryState, NotKeyed};
+use governor::{Quota, RateLimiter};
 use itertools::Itertools;
 use log::{error, info, warn};
 use primitive_types::U256;
@@ -20,7 +19,7 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
 use const_format::formatcp;
 use tokio::sync::mpsc::{Permit, PermitIterator, Receiver, Sender, channel, error::TrySendError};
-use tokio::time::{Duration, Instant, sleep, sleep_until};
+use tokio::time::{Duration, Instant, sleep};
 use tokio::{select, task};
 use tokio::sync::OnceCell;
 
@@ -37,6 +36,8 @@ const U_RESULTS_PER_PAGE: usize = 1;
 const CHECK_ID_URL_BASE: &str = "https://factordb.com/index.php?open=Prime&ct=Proof&id=";
 const PRP_TASK_BUFFER_SIZE: usize = 2 * PRP_RESULTS_PER_PAGE;
 const U_TASK_BUFFER_SIZE: usize = 16;
+const C_TASK_BUFFER_SIZE: usize = 8192;
+const C_RESULTS_PER_PAGE: usize = 5000;
 const MIN_CAPACITY_AT_PRP_RESTART: usize = PRP_TASK_BUFFER_SIZE - PRP_RESULTS_PER_PAGE / 2;
 const MIN_CAPACITY_AT_U_RESTART: usize = U_TASK_BUFFER_SIZE / 2;
 const PRP_SEARCH_URL_BASE: &str = formatcp!(
@@ -44,6 +45,9 @@ const PRP_SEARCH_URL_BASE: &str = formatcp!(
 );
 const U_SEARCH_URL_BASE: &str = formatcp!(
     "https://factordb.com/listtype.php?t=2&mindig={MIN_DIGITS_IN_U}&perpage={U_RESULTS_PER_PAGE}&start="
+);
+const C_SEARCH_URL: &str = formatcp!(
+    "https://factordb.com/listtype.php?t=3&perpage={C_RESULTS_PER_PAGE}"
 );
 static EXIT_TIME: OnceCell<Instant> = OnceCell::const_new();
 
@@ -59,10 +63,13 @@ struct CheckTask {
     task_type: CheckTaskType,
 }
 
+type SimpleRateLimiter = Arc<RateLimiter<NotKeyed, InMemoryState, DefaultClock>>;
+
 struct BuildTaskContext {
     http: Client,
     bases_regex: Regex,
-    rps_limiter: Arc<RateLimiter<NotKeyed, InMemoryState, DefaultClock>>,
+    rps_limiter: SimpleRateLimiter,
+    c_receiver: Receiver<u128>
 }
 
 fn count_ones(u256: U256) -> u32 {
@@ -100,19 +107,32 @@ async fn try_get_and_decode(http: &Client, url: &str) -> Option<Box<str>> {
     None
 }
 
-async fn get_prp_remaining_bases(id: &str, ctx: &BuildTaskContext) -> U256 {
+async fn composites_while_waiting(end: Instant, http: &Client, c_receiver: &mut Receiver<u128>, rps_limiter: &SimpleRateLimiter) {
+    while Instant::now() < end {
+        let id = c_receiver.recv().await.unwrap();
+        rps_limiter.until_ready().await;
+        if let Err(e) = http.get(format!("https://factordb.com/sequences.php?check={id}")).send().await {
+            error!("Error while checking composite with ID {id}: {e}");
+        } else {
+            info!("Checked composite with ID {id}");
+        }
+    }
+}
+
+async fn get_prp_remaining_bases(id: &str, ctx: &mut BuildTaskContext) -> U256 {
     let BuildTaskContext {
         http,
         bases_regex,
         rps_limiter,
+        c_receiver
     } = ctx;
     let mut bases_left = U256::MAX - 3;
     let bases_url = format!("{CHECK_ID_URL_BASE}{id}");
     rps_limiter.until_ready().await;
-    let bases_text = retrying_get_and_decode(http, &bases_url, RETRY_DELAY).await;
+    let bases_text = retrying_get_and_decode(&http, &bases_url, RETRY_DELAY).await;
     if !bases_text.contains("&lt;") {
         error!("ID {id}: Failed to decode status: {bases_text}");
-        sleep(UNPARSEABLE_RESPONSE_RETRY_DELAY).await;
+        composites_while_waiting(Instant::now() + UNPARSEABLE_RESPONSE_RETRY_DELAY, http, c_receiver, &rps_limiter).await;
         return U256::from(0);
     }
     if bases_text.contains(" is prime") || !bases_text.contains("PRP") {
@@ -149,8 +169,9 @@ async fn do_checks(
     prp_sender: Sender<CheckTask>,
     mut prp_receiver: Receiver<CheckTask>,
     mut u_receiver: Receiver<CheckTask>,
+    c_receiver: Receiver<u128>,
     http: Client,
-    rps_limiter: Arc<RateLimiter<NotKeyed, InMemoryState, DefaultClock>>,
+    rps_limiter: SimpleRateLimiter,
 ) {
     let mut next_unknown_attempt = Instant::now();
     let mut retry = None;
@@ -162,14 +183,16 @@ async fn do_checks(
             .dot_matches_new_line(true)
             .build()
             .unwrap();
-    let ctx = BuildTaskContext {
+    let mut ctx = BuildTaskContext {
         bases_regex: Regex::new("Bases checked[^\n]*\n[^\n]*(?:([0-9]+),? )+").unwrap(),
         http: http.clone(),
         rps_limiter: rps_limiter.clone(),
+        c_receiver
     };
     let mut bases_before_next_cpu_check = 1;
     throttle_if_necessary(
         &http,
+        &mut ctx.c_receiver,
         &rps_limiter,
         &resources_regex,
         &mut bases_before_next_cpu_check,
@@ -181,7 +204,7 @@ async fn do_checks(
         match task_type {
             CheckTaskType::Prp => {
                 let mut stopped_early = false;
-                let bases_left = get_prp_remaining_bases(&id.to_string(), &ctx).await;
+                let bases_left = get_prp_remaining_bases(&id.to_string(), &mut ctx).await;
                 if bases_left == U256::from(0) {
                     continue;
                 }
@@ -195,11 +218,12 @@ async fn do_checks(
                     let text = retrying_get_and_decode(&http, &url, RETRY_DELAY).await;
                     if !text.contains(">number<") {
                         error!("Failed to decode result from {url}: {text}");
-                        sleep(UNPARSEABLE_RESPONSE_RETRY_DELAY).await;
+                        composites_while_waiting(Instant::now() + UNPARSEABLE_RESPONSE_RETRY_DELAY, &http, &mut ctx.c_receiver, &rps_limiter).await;
                         break;
                     }
                     throttle_if_necessary(
                         &http,
+                        &mut ctx.c_receiver,
                         &rps_limiter,
                         &resources_regex,
                         &mut bases_before_next_cpu_check,
@@ -230,6 +254,7 @@ async fn do_checks(
                         {
                             if try_handle_unknown(
                                 &http,
+                                &mut ctx.c_receiver,
                                 &u_status_regex,
                                 &many_digits_regex,
                                 id,
@@ -250,6 +275,7 @@ async fn do_checks(
                                 };
                                 if !try_handle_unknown(
                                     &http,
+                                    &mut ctx.c_receiver,
                                     &u_status_regex,
                                     &many_digits_regex,
                                     id,
@@ -273,6 +299,7 @@ async fn do_checks(
             CheckTaskType::U => {
                 throttle_if_necessary(
                     &http,
+                    &mut ctx.c_receiver,
                     &rps_limiter,
                     &resources_regex,
                     &mut bases_before_next_cpu_check,
@@ -281,6 +308,7 @@ async fn do_checks(
                 .await;
                 if !try_handle_unknown(
                     &http,
+                    &mut ctx.c_receiver,
                     &u_status_regex,
                     &many_digits_regex,
                     id,
@@ -301,81 +329,72 @@ async fn do_checks(
     }
 }
 
-async fn try_handle_unknown<
-    S: DirectStateStore,
-    T: ReasonablyRealtime,
-    U: RateLimitingMiddleware<T::Instant, NegativeOutcome = NotUntil<T::Instant>>,
->(
+async fn try_handle_unknown(
     http: &Client,
+    c_receiver: &mut Receiver<u128>,
     u_status_regex: &Regex,
     many_digits_regex: &Regex,
     id: u128,
     next_attempt: &mut Instant,
     source_file: Option<u64>,
-    rate_limiter: &Arc<RateLimiter<NotKeyed, S, T, U>>,
+    rate_limiter: &SimpleRateLimiter,
 ) -> bool {
     let remaining_wait = next_attempt.saturating_duration_since(Instant::now());
     if remaining_wait > UNKNOWN_STATUS_CHECK_MAX_BLOCKING_WAIT {
-        let remaining_secs = remaining_wait.as_secs();
-        warn!("Waiting {remaining_secs} seconds to start an unknown-status task");
-        false
-    } else {
-        if remaining_wait > Duration::ZERO {
+        composites_while_waiting(*next_attempt, http, c_receiver, rate_limiter).await;
+    } else if remaining_wait > Duration::ZERO {
         sleep(remaining_wait).await;
-            }
-        let url = format!("https://factordb.com/index.php?id={id}&prp=Assign+to+worker");
-        rate_limiter.until_ready().await;
-        let result = retrying_get_and_decode(&http, &url, RETRY_DELAY).await;
-        if let Some(status) = u_status_regex.captures_iter(&result).next() {
-            match status.get(1) {
-                None => {
-                    if many_digits_regex.is_match(&result) {
-                        warn!("Unknown-status number {id} is too large for a PRP check!");
-                        // FIXME: Should restart search if this number came from a search
-                        true
-                    } else {
-                        error!("Failed to decode status for {id}: {result}");
-                        sleep(UNPARSEABLE_RESPONSE_RETRY_DELAY).await;
-                        false
-                    }
+    }
+    let url = format!("https://factordb.com/index.php?id={id}&prp=Assign+to+worker");
+    rate_limiter.until_ready().await;
+    let result = retrying_get_and_decode(&http, &url, RETRY_DELAY).await;
+    if let Some(status) = u_status_regex.captures_iter(&result).next() {
+        match status.get(1) {
+            None => {
+                if many_digits_regex.is_match(&result) {
+                    warn!("Unknown-status number {id} is too large for a PRP check!");
+                    // FIXME: Should restart search if this number came from a search
+                    true
+                } else {
+                    error!("Failed to decode status for {id}: {result}");
+                    composites_while_waiting(Instant::now() + UNPARSEABLE_RESPONSE_RETRY_DELAY,
+                    http, c_receiver, rate_limiter).await;
+                    false
                 }
-                Some(matched_status) => match matched_status.as_str() {
-                    "Assigned" => {
-                        info!("Assigned PRP check for unknown-status number with ID {id} from dump file {source_file:?}");
-                        true
-                    }
-                    "Please wait" => {
-                        warn!("Got 'please wait' for unknown-status number with ID {id}");
-                        *next_attempt = Instant::now() + UNKNOWN_STATUS_CHECK_BACKOFF;
-                        false
-                    }
-                    _ => {
-                        warn!("Unknown-status number with ID {id} from dump file {source_file:?} is already being checked");
-                        true
-                    }
-                },
             }
+            Some(matched_status) => match matched_status.as_str() {
+                "Assigned" => {
+                    info!("Assigned PRP check for unknown-status number with ID {id} from dump file {source_file:?}");
+                    true
+                }
+                "Please wait" => {
+                    warn!("Got 'please wait' for unknown-status number with ID {id}");
+                    *next_attempt = Instant::now() + UNKNOWN_STATUS_CHECK_BACKOFF;
+                    false
+                }
+                _ => {
+                    warn!("Unknown-status number with ID {id} from dump file {source_file:?} is already being checked");
+                    true
+                }
+            },
+        }
+    } else {
+        if many_digits_regex.is_match(&result) {
+            warn!("Unknown-status number {id} is too large for a PRP check!");
+            // FIXME: Should restart search if this number came from a search
+            true
         } else {
-            if many_digits_regex.is_match(&result) {
-                warn!("Unknown-status number {id} is too large for a PRP check!");
-                // FIXME: Should restart search if this number came from a search
-                true
-            } else {
-                error!("Failed to decode status for {id} from result: {result}");
-                sleep(UNPARSEABLE_RESPONSE_RETRY_DELAY).await;
-                false
-            }
+            error!("Failed to decode status for {id} from result: {result}");
+            *next_attempt = Instant::now() + UNPARSEABLE_RESPONSE_RETRY_DELAY;
+            false
         }
     }
 }
 
-async fn throttle_if_necessary<
-    S: DirectStateStore,
-    T: ReasonablyRealtime,
-    U: RateLimitingMiddleware<T::Instant, NegativeOutcome = NotUntil<T::Instant>>,
->(
+async fn throttle_if_necessary(
     http: &Client,
-    rps_limiter: &Arc<RateLimiter<NotKeyed, S, T, U>>,
+    c_receiver: &mut Receiver<u128>,
+    rps_limiter: &SimpleRateLimiter,
     resources_regex: &Regex,
     bases_before_next_cpu_check: &mut u64,
     sleep_first: bool,
@@ -385,7 +404,7 @@ async fn throttle_if_necessary<
         return;
     }
     if sleep_first {
-        sleep(Duration::from_secs(10)).await; // allow for delay in CPU accounting
+        composites_while_waiting(Instant::now() + Duration::from_secs(10), http, c_receiver, rps_limiter).await; // allow for delay in CPU accounting
     }
     rps_limiter.until_ready().await;
     let resources_text = retrying_get_and_decode(&http, "https://factordb.com/res.php", RETRY_DELAY).await;
@@ -429,7 +448,7 @@ async fn throttle_if_necessary<
             warn!("Throttling won't end before program exit; exiting now");
             exit(0);
         }
-        sleep_until(cpu_reset_time).await;
+        composites_while_waiting(cpu_reset_time, http, c_receiver, rps_limiter).await;
         *bases_before_next_cpu_check = MAX_BASES_BETWEEN_RESOURCE_CHECKS;
         CPU_TENTHS_SPENT_LAST_CHECK.store(0, Ordering::Release);
     } else {
@@ -478,11 +497,13 @@ async fn main() {
         .unwrap();
     let (prp_sender, prp_receiver) = channel(PRP_TASK_BUFFER_SIZE);
     let (u_sender, u_receiver) = channel(U_TASK_BUFFER_SIZE);
+    let (c_sender, c_receiver) = channel(C_TASK_BUFFER_SIZE);
     let rps_limiter = Arc::new(rps_limiter);
     task::spawn(do_checks(
         prp_sender.clone(),
         prp_receiver,
         u_receiver,
+        c_receiver,
         http.clone(),
         rps_limiter.clone(),
     ));
@@ -585,6 +606,17 @@ async fn main() {
                 if u_start > MAX_START {
                     info!("Restarting U search: searched {u_start} unknowns");
                     restart_u = true;
+                }
+            }
+            _ = c_sender.reserve_many(C_RESULTS_PER_PAGE) => {
+                let composites_page = retrying_get_and_decode(&http, C_SEARCH_URL, RETRY_DELAY).await;
+                for c_id in id_regex.captures_iter(&composites_page) {
+                    let c_id = c_id.get(1).unwrap().as_str();
+                    let Ok(c_id) = c_id.parse::<u128>() else {
+                        error!("Invalid composite number ID in search results: {}", c_id);
+                        continue;
+                    };
+                    c_sender.send(c_id).await.unwrap();
                 }
             }
         }
