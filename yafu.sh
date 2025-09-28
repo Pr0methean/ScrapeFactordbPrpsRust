8cb34db5f4dca962d08ef1034b0e1de66c10cd45
hennickc <hennickc@amazon.com>
2025-09-28 13:55:50 -0700

    yafu.sh: let yafu use 2 threads

--- yafu.sh
+++ yafu.sh
@@ -31,7 +31,7 @@ while read -r num; do
         fi
       done < <(
         echo "$(date -Is): Factoring ${num} with yafu" >&2
-        ./yafu -threads 1 -R -qssave "./qs" -session "./session" -logfile "./log" -o "./nfs" <<<"factor(${num})" 2>&1 \
+        ./yafu -threads 2 -R -qssave "./qs" -session "./session" -logfile "./log" -o "./nfs" <<<"factor(${num})" 2>&1 \
           | tee "./out" \
           | grep '\(^P[0-9]\|factor = \)' | grep -o '= [0-9]\+' | grep -o '[0-9]\+' \
           | head -n -1 | uniq
