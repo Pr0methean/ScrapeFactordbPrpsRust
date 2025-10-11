12f1ffc0833ec738664ac7f1092b3b145fbcf121
hennickc <hennickc@amazon.com>
2025-10-10 23:51:57 -0700

    yafu.sh: Remove redundant timestamps

--- yafu.sh
+++ yafu.sh
@@ -15,8 +15,7 @@ while read -r num; do
       declare factor
       start_time="$(date +%s%N)"
       while read -r factor; do (
-        now="$(date -Is)"
-        echo "${now}: Found factor ${factor} of ${num}"
+        echo "Found factor ${factor} of ${num}"
         output=$(curl -X POST --retry 10 --retry-all-errors --retry-delay 10 http://factordb.com/reportfactor.php -d "number=${num}&factor=${factor}")
         error=$?
         if ! grep -q "submitted" <<< "$output"; then
@@ -34,16 +33,16 @@ while read -r num; do
           fi
         fi
       )& done < <(
-        echo "$(date -Is): Factoring ${num} with yafu" >&2
+        echo "Factoring ${num} with yafu" >&2
         ./yafu -threads 2 -R -qssave "./qs" -session "./session" -logfile "./log" -o "./nfs" <<<"factor(${num})" 2>&1 \
           | tee "./out" \
           | grep '\(^P[0-9]\|factor = \)' | grep -o '= [0-9]\+' | grep -o '[0-9]\+' \
           | head -n -1 | uniq
         if grep -q '^P[0-9]' "./out"; then
           end_time=$(date +%s%N)
-          echo "$(date -Is): Done factoring ${num} with yafu after $(./format-nanos.sh $((end_time - start_time)))" >&2
+          echo "Done factoring ${num} with yafu after $(./format-nanos.sh $((end_time - start_time)))" >&2
         else
-          echo "$(date -Is): Failed to factor ${num} with yafu" >&2
+          echo "Failed to factor ${num} with yafu" >&2
           tail "./out" >&2
         fi
         rm "./out"
