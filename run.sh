2a789f1d76c28b29aac8db5a2521f8000d986ad2
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2025-12-03 06:52:42 -0800

    Update run.sh to not override RUSTFLAGS

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@
 # sudo apt-get install libecm-dev gmp-ecm cargo
 mkfifo composites || true
 ./build.sh
-RUSTFLAGS="-C target-cpu=native" RUST_BACKTRACE=full cargo +nightly run --release 2>&1 | tee /tmp/rust.log | sed -E 's/(.{1000}).*$/\1.../' &
+RUST_BACKTRACE=full nice -+19 ./target/release/ScrapeFactordbPrpsRust 2>&1 | tee /tmp/rust.log | sed -E 's/(.{1000}).*$/\1.../' &
 ./yafu.sh <composites 2>&1 | tee /tmp/yafu.log &
