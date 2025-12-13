482f6989276d1514ba3bb9544a4fde011b37a5ee
hennickc <hennickc@amazon.com>
2025-12-13 14:26:30 -0800

    Tweak: restore approximate largest-first ordering in find_unique_factors output

--- src/algebraic.rs
+++ src/algebraic.rs
@@ -2883,7 +2883,8 @@ pub fn find_unique_factors(expr: &Factor) -> Box<[Factor]> {
                     factors.iter().join(", ")
                 );
             }
-            let factors: Box<[Factor]> = factors.into_iter().collect();
+            let mut factors: Box<[Factor]> = factors.into_iter().collect();
+            factors.reverse();
             UNIQUE_FACTOR_CACHE
                 .with_borrow_mut(|cache| cache.insert(expr.clone(), factors.clone()));
             factors
