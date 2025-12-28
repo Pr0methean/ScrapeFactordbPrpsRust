606b81ef7b5c16a19e4a7c2d5b4a6fc3af3e92c6
hennickc <hennickc@amazon.com>
2025-12-28 12:03:17 -0800

    Revert "Add logging to process_divisibility_worklist to diagnose spurious cancellation"
    This reverts commit a83a22bd

--- src/graph.rs
+++ src/graph.rs
@@ -147,7 +147,6 @@ impl FactorData {
             if !already_processed.insert(item.clone()) {
                 continue;
             }
-            info!("process_divisibility_worklist: {item:?}");
             match item {
                 WorkItem::Propagate {
                     factor,
